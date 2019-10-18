17030713a53483bf0e714a6b8c3cf9c6ccaaae26
Daniyal Maniar <16mdm13@caslab.queensu.ca>
2019-10-18 19:56:58 -0400

    Lab2 code written

--- lab2a/lab2.c
+++ lab2a/lab2.c
@@ -10,26 +10,35 @@
 #include <linux/proc_fs.h>
 #include <linux/sched.h>
 
+#define NR_THREADS 0Xc038b3a8
+
 static struct task_struct * firstTask, *lastTask;
 
 int cnt;
 
 int my_read_proc(char * page, char **start, off_t fpos, int blen, int * eof, void * data){
-
-    int numChars;
+    int numChars = 0;
     if (fpos == 0){
-	     // write headers
-	    // find first task
-        // write first task
-        // advance to next task
+	     numChars += sprintf(page, "Number of running processes: %d\n", nr_running);
+	     numChars += sprintf(page + numChars, "Number of running threads: %d\n", (int*)NR_THREADS);
+	     numChars += sprintf(page + numChars, "PID\tUID\tNICE\n");
+	     // First Task
+	     theTask = &init_task;
+	     lastTask = theTask;
+	     if (theTask -> pid != 0){
+	         numChars += sprintf(page + numChars, "%d\t%d\t%d\n", theTask->pid, theTask->uid, theTask->nice);
+	     }
+	     lastTask = lastTask->next_task;
     } else {
-        if (at back at begining of list){
+        if (lastTask == theTask){
             *eof = 0;
             *start = page;
             return 0;
         }
-	    // write task info for one task
-	    // advance to next task
+        if (theTask -> pid != 0){
+            numChars += sprintf(page + numChars, "%d\t%d\t%d\n", theTask->pid, theTask->uid, theTask->nice);
+        }
+        lastTask = lastTask->next_task;
     }
     *eof = 1;
     *start = page;
@@ -38,7 +47,18 @@ int my_read_proc(char * page, char **start, off_t fpos, int blen, int * eof, voi
 
 int init_module(){
    struct proc_dir_entry * proc_entry;
+   char [] procName = "lab2"
+   proc_entry = create_proc_entry("lab2", 0444, NULL);
+   if (proc_entry == NULL) {
+        remove_proc_entry("lab2", &proc_root);
+        printk(KERN_ALERT "Error: Could not initialize /proc/%s\n",
+                procfs_name);
+        return -ENOMEM;
+   }
+   proc_entry->read_proc = my_read_proc;
+   return 0
 }
 
 void cleanup_module(){
+    remove_proc_entry("lab2", &proc_root);
 }
