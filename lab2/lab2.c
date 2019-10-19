4750e32314286dc4dd55c20008783a27fe261f00
Daniyal Maniar <16mdm13@caslab.queensu.ca>
2019-10-18 20:06:47 -0400

    Updated the repo

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -1,37 +1,64 @@
+/*+
+ * Module:  lab2.c
+ *
+ * Purpose: Skeleton solution to ELEC 377 Lab2.
+ *
+-*/
+
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/proc_fs.h>
 #include <linux/sched.h>
 
-#define nr_threads_LOC 0Xc038b3a8
+#define NR_THREADS 0Xc038b3a8
 
-int *nr_threads_ptr = (int*)nr_threads_LOC;
+static struct task_struct * firstTask, *lastTask;
 
-static struct task_struct *firstTask, *nextTask;
+int cnt;
 
-int my_read_proc(...) { 
-	if  (file_ops == 0) {
-		//write the header to the buffer
-		//find the first valid task (init_task)
-		//add the first task information to the buffer
-		//go to the second task
-	} else {
-		if (we are at beginning of list again) {
-			*eof = 1;
-			*start = page;
-			return 0;
-		}
-		//write task info to the buffer
-		//advance the task pointer
-	}
-	*eof = 1;
-	*start = page;
-	return numChars;
+int my_read_proc(char * page, char **start, off_t fpos, int blen, int * eof, void * data){
+    int numChars = 0;
+    if (fpos == 0){
+	     numChars += sprintf(page, "Number of running processes: %d\n", nr_running);
+	     numChars += sprintf(page + numChars, "Number of running threads: %d\n", (int*)NR_THREADS);
+	     numChars += sprintf(page + numChars, "PID\tUID\tNICE\n");
+	     // First Task
+	     theTask = &init_task;
+	     lastTask = theTask;
+	     if (theTask -> pid != 0){
+	         numChars += sprintf(page + numChars, "%d\t%d\t%d\n", theTask->pid, theTask->uid, theTask->nice);
+	     }
+	     lastTask = lastTask->next_task;
+    } else {
+        if (lastTask == theTask){
+            *eof = 0;
+            *start = page;
+            return 0;
+        }
+        if (theTask -> pid != 0){
+            numChars += sprintf(page + numChars, "%d\t%d\t%d\n", theTask->pid, theTask->uid, theTask->nice);
+        }
+        lastTask = lastTask->next_task;
+    }
+    *eof = 1;
+    *start = page;
+    return numChars;
 }
-int init_module() {
-	struct prov_dir_entry * proc_entry;
+
+int init_module(){
+   struct proc_dir_entry * proc_entry;
+   char [] procName = "lab2"
+   proc_entry = create_proc_entry("lab2", 0444, NULL);
+   if (proc_entry == NULL) {
+        remove_proc_entry("lab2", &proc_root);
+        printk(KERN_ALERT "Error: Could not initialize /proc/%s\n",
+                procfs_name);
+        return -ENOMEM;
+   }
+   proc_entry->read_proc = my_read_proc;
+   return 0
 }
 
-void init_module() {
-	//todo
+void cleanup_module(){
+    remove_proc_entry("lab2", &proc_root);
 }
