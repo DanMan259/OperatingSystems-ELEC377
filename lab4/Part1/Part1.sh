c3c85d7c3c1352fcaba3ee735e1d65e78016de42
Daniyal Maniar (16MDM13) <daniyal.maniar@queensu.ca>
2019-11-16 21:52:05 -0500

    Update Part1.sh

--- lab4/Part1/Part1.sh
+++ lab4/Part1/Part1.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-
-echo "PID\t\tUSER\t\tRSS\t\tCOMMAND"
+echo -e "PID\t\tUSER\tRSS\tCOMMAND" > test1.txt
 
 for FILE in $(ls -vd /proc/[1-9]*); do
-	echo $FILE
-done
+    if test -f "$FILE/status"; then
+        echo $FILE
+        cat $FILE/status | grep -w Pid: >> test1.txt
+        sed -i 's/Pid:\t//' test1.txt
+        truncate -s-1 test1.txt
+        echo -ne "\t\t$(stat -c %U $FILE)\t" >> test1.txt
+        cat $FILE/status | grep -w VmRSS: >> test1.txt
+        sed -i 's/VmRSS:\t//' test1.txt
+        truncate -s-4 test1.txt
+        echo -e "\t$(cat $FILE/cmdline)" >> test1.txt
+    fi
+done 
+
+cat test1.txt
