accfd391f04a01df44961c21929ba555f5e701d0
Daniyal Maniar (16MDM13) <daniyal.maniar@queensu.ca>
2019-11-17 23:16:44 -0500

    Update lab4/Part1/Part1.sh

--- lab4/Part1/Part1.sh
+++ lab4/Part1/Part1.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
-# print banner to console
+# Print banner to console
 echo -e "PID\tUSER\tRSS\tCOMMAND"
 
-# only loop through files in proc directory that consist of digits only
+# Only loop through files in proc directory that consist of digits only
 for FILE in $(ls -vd /proc/[1-9]*); do
     # if process has a 'status' directory in it
     if test -f "$FILE/status"; then
-        # get PID for proc
+        # Get PID for proc
         PID=${FILE/\/proc\//}
-        # get user using the stat command
+        # Get user using the stat command
         USER=$(stat -c %U $FILE)
-        # get RSS value and remove 'VmRSS' and 'kB'
+        # Get RSS value and remove 'VmRSS' and 'kB'
         RSS=$(grep -w VmRSS: $FILE/status | sed -e 's/VmRSS:\t *//'| sed -e 's/ [kK]B//')
-        # check if RSS is empty
+        # Check if RSS is empty
         if test -z "$RSS"; then
             RSS=0 
         fi
-        # get command from 'cmdline' in proc
+        # Get command from 'cmdline' in proc
         CMD=$(cat $FILE/cmdline | tr '\0' ' ')
+        # Check if CMD is empty if it is then check the status files for the details
         if test -z "$CMD"; then
             CMD="[$(grep 'Name' $FILE/'status' | sed -e 's/.*\t *//')]"
         fi
+        # Print the line with the process information
         echo -e "$PID\t$USER\t$RSS\t$CMD"
     fi
 done 
