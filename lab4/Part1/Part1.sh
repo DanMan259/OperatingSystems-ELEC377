585a5d8ca62aa713e627baacc7d3e738652bb600
Daniyal Maniar (16MDM13) <daniyal.maniar@queensu.ca>
2019-11-17 20:52:33 -0500

    Update Part1.sh

--- lab4/Part1/Part1.sh
+++ lab4/Part1/Part1.sh
@@ -4,13 +4,13 @@ echo -e "PID\t\tUSER\tRSS\tCOMMAND" > test1.txt
 
 for FILE in $(ls -vd /proc/[1-9]*); do
     if test -f "$FILE/status"; then
-        PID = {$FILE/\/proc\/}
-        USER = $(stat -c %U $FILE)
-        RSS = $(cat $FILE/status | grep -w VmRSS: | sed -e 's/VmRSS:\t *//')
+        PID={$FILE/\/proc\/}
+        USER=$(stat -c %U $FILE)
+        RSS=$(cat $FILE/status | grep -w VmRSS: | sed -e 's/VmRSS:\t *//')
         if [ -z "$RSS"]; then
-            RSS = 0 
+            RSS=0 
         fi
-        CMD = $(cat $FILE/cmdline | tr '\0' ' ')
+        CMD=$(cat $FILE/cmdline | tr '\0' ' ')
         echo -e '$PID\t\t$USER\t$RSS\t$CMD'
     fi
 done 
