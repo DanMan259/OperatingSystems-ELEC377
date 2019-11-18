21be9be8c685540b5e2abdddb07387e6e66d2ffc
group-E377-Wed-57 <group-E377-Wed-57@elec377.queensu.ca>
2019-11-17 20:33:57 -0500

    Added test

--- lab4/Part1/Part1.sh
+++ lab4/Part1/Part1.sh
@@ -7,11 +7,11 @@ for FILE in $(ls -vd /proc/[1-9]*); do
         echo $FILE
         cat $FILE/status | grep -w Pid: >> test1.txt
         sed -i 's/Pid:\t//' test1.txt
-        truncate -s-1 test1.txt
+        tr -t -s-1 test1.txt
         echo -ne "\t\t$(stat -c %U $FILE)\t" >> test1.txt
         cat $FILE/status | grep -w VmRSS: >> test1.txt
         sed -i 's/VmRSS:\t//' test1.txt
-        truncate -s-4 test1.txt
+        tr -t -s-4 test1.txt
         echo -e "\t$(cat $FILE/cmdline)" >> test1.txt
     fi
 done 
