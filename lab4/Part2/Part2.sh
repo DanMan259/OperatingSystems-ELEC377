8cddd59aa4df62f1c066747f6151b8823fea5215
Daniyal Maniar (16MDM13) <daniyal.maniar@queensu.ca>
2019-11-17 22:08:40 -0500

    Update lab4/Part2/Part2.sh

---
+++ lab4/Part2/Part2.sh
@@ -0,0 +1,21 @@
+MAINFILES = ""
+MODFILES = ""
+OTHERFILES = ""
+for FILE in $(find $1 -name "*.c"); do
+    if grep -wq "int main" $FILE; then
+        MAINFILES = "$MAINFILES$FILE:$(cat $FILE | grep printf | wc -l),$(cat $FILE | grep fprintf | wc -l)\n"
+    elif grep -wq "init_module" $FILE; then
+        MODFILES = "$MODILES$FILE:$(grep -n "printk" $FILE | sed -e 's/:.*//' | tr '\n' ',')\n"
+    else
+        OTHERFILES = "#OTHERFILES$FILE\n"
+    fi
+if test -z $MAINFILES; then
+    MAINFILES = "No main files"
+fi
+if test -z $MODFILES; then
+    MODFILES = "No module files"
+fi
+if test -z $OTHERFILES; then
+    OTHERFILES = "No other files"
+fi
+echo -e "Main Files:\n$MAINFILES Module Files:\n$MODFILES Other Files:\n$OTHERFILES"
