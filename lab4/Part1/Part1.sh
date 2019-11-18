a8456da28ac7b2c320f9e8fca3ac0b45d7f8ded6
Nicholas Alderman (16NAA5) <16naa5@queensu.ca>
2019-11-17 23:05:17 -0500

    Update Part1.sh

--- lab4/Part1/Part1.sh
+++ lab4/Part1/Part1.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+# print banner to console
 echo -e "PID\tUSER\tRSS\tCOMMAND"
 
+# only loop through files in proc directory that consist of digits only
 for FILE in $(ls -vd /proc/[1-9]*); do
+    # if process has a 'status' directory in it
     if test -f "$FILE/status"; then
+        # get PID for proc
         PID=${FILE/\/proc\//}
+        # get user using the stat command
         USER=$(stat -c %U $FILE)
+        # get RSS value and remove 'VmRSS' and 'kB'
         RSS=$(cat $FILE/status | grep -w VmRSS: | sed -e 's/VmRSS:\t *//'| sed -e 's/ [kK]B//')
+        # check if RSS is empty
         if test -z "$RSS"; then
             RSS=0 
         fi
+        # get command from 'cmdline' in proc
         CMD=$(cat $FILE/cmdline | tr '\0' ' ')
         echo -e "$PID\t$USER\t$RSS\t$CMD"
     fi
