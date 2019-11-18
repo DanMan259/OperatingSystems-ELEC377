36cf00a084a40c8b52aa61e69f20bab6a29b126d
Daniyal Maniar (16MDM13) <daniyal.maniar@queensu.ca>
2019-11-17 21:00:40 -0500

    Update Part1.sh

--- lab4/Part1/Part1.sh
+++ lab4/Part1/Part1.sh
@@ -6,7 +6,7 @@ for FILE in $(ls -vd /proc/[1-9]*); do
     if test -f "$FILE/status"; then
         PID=${FILE/\/proc\//}
         USER=$(stat -c %U $FILE)
-        RSS=$(cat $FILE/status | grep -w VmRSS: | sed -e 's/VmRSS:\t *//'|sed -e 's/ [kK]B//')
+        RSS=$(cat $FILE/status | grep -w VmRSS: | sed -e 's/VmRSS:\t *//'| sed -e 's/ [kK]B//')
         if test -z "$RSS"; then
             RSS=0 
         fi
