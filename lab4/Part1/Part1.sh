f16e5877f446dbd8c146fd2c74f792dba00e9bed
Daniyal Maniar (16MDM13) <daniyal.maniar@queensu.ca>
2019-11-17 20:57:36 -0500

    Update Part1.sh

--- lab4/Part1/Part1.sh
+++ lab4/Part1/Part1.sh
@@ -4,7 +4,7 @@ echo -e "PID\t\tUSER\tRSS\tCOMMAND"
 
 for FILE in $(ls -vd /proc/[1-9]*); do
     if test -f "$FILE/status"; then
-        PID= $FILE/\/proc\/
+        PID=${FILE/\/proc\//}
         USER=$(stat -c %U $FILE)
         RSS=$(cat $FILE/status | grep -w VmRSS: | sed -e 's/VmRSS:\t *//')
         if test -z "$RSS"; then
