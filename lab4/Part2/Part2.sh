35db516e9e27fb943db15a1d9f3e5b69126a400c
Daniyal Maniar (16MDM13) <daniyal.maniar@queensu.ca>
2019-11-17 22:41:58 -0500

    Update lab4/Part2/Part2.sh

--- lab4/Part2/Part2.sh
+++ lab4/Part2/Part2.sh
@@ -5,7 +5,7 @@ MODFILES=""
 OTHERFILES=""
 for FILE in $(find $1 -type f -name "*.c"); do
     if grep -wq "int main" $FILE; then
-        MAINFILES="$MAINFILES$FILE:$(cat $FILE | grep printf | wc -l),$(cat $FILE | grep fprintf | wc -l)\n"
+        MAINFILES="$MAINFILES$FILE:$(cat $FILE | grep -w printf | wc -l),$(cat $FILE | grep -w fprintf | wc -l)\n"
     elif grep -wq "init_module" $FILE; then
         MODFILES="$MODFILES$FILE:$(grep -n "printk" $FILE | sed -e 's/:.*//' | tr '\n' ',' | sed -e 's/,$//')\n"
     else
